b724f2fd8bf437e62fb4f408a529e71217e623d1
parkyunhwan <parky8618@q.ssu.ac.kr>
2020-01-10 23:51:23 +0900

    mod tetromino

--- tetromino.cpp
+++ tetromino.cpp
@@ -4,10 +4,23 @@ using namespace std;
 
 int main()
 {
-	int rec, squ, nen;
-	int paper;
-	int a, b;
-	cin >> a >> b;
+	int M, N;
+	cin >> M >> N;
+	int** arr = new int*[M];
+	for(int i=0; i < M; i++)
+	{
+		arr[i] = new int[N];
+	}
+
+	for(int i=0; i < M; i++)
+	{
+		for(int j=0; j < M; j++)
+		{
+			int in;
+			cin >> in;
+			arr[i][j]=in;
+		}
+	}
 
 		
 }
