dc9a456cb4aafa92f83d46f6c6002055a69f07d3
parkyunhwan <parky8618@q.ssu.ac.kr>
2020-02-06 02:32:22 +0900

    create goldbach.cpp (time_over)

---
+++ math/goldbach_s_guess.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+#define MAX 100000
+int num[MAX];
+bool prin[MAX];
+int n1,n2;
+bool prime(int n)
+{
+    if(n < 2)
+        return false;
+    for(int i=2; i*i < n; i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+void check(int n)
+{
+    for(int i=0; i < n; i++)
+    {
+        bool c;
+        c=prime(i);
+        if(c)
+            prin[i]=true;
+    }
+}
+
+void goldbach(int n)
+{
+    check(n);
+    for(int i=n; i >= 2; i--)
+    {
+        if(prin[i]==true)
+            n1 = prin[i];
+        else
+            continue;
+        for(int j=i; i >=2; j--)
+        {
+            if(prin[j]==true)
+                n2 = prin[j];
+            else
+                continue;
+            if(n == (n1+n2))
+                break;
+        }
+        if(n == (n1+n2))
+            break;
+        
+    }
+    cout << n << "=" << n1 << "+" << n2 << endl;
+}
+int main()
+{
+    int i=0;
+    while(1)
+    {
+        cin >> num[i];
+        if(num[i]==0)
+            break;
+        i++;
+    }
+    i=0;
+    while(1)
+    {
+        if(num[i]==0)
+            break;
+        goldbach(num[i]);
+        i++;
+    }
+}
