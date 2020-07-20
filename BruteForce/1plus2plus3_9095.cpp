c2be0183cff3a72fe1c22c01356e84d6d02b48e6
YunHwan <pyh8618@gmail.com>
2020-07-20 23:56:56 +0900

    add temp 123plus

---
+++ BruteForce/1plus2plus3_9095.cpp
@@ -0,0 +1,28 @@
+/*
+    20-07-08
+    9095 1,2,3 더하기
+
+    생각단계
+    1. 1만 사용 할 때 1, 2 만 사용할 때 모두 사용할 때로 경우를 나눈다.
+    2. 되는 경우마다 갯수를 증가시킨다.
+
+    구현단계
+
+*/
+#include <iostream>
+using namespace std;
+void solve()
+{
+    int casenum;
+    scanf("%d", &casenum);
+    while(1){
+        
+    }
+}
+int testn;
+int main()
+{
+    scanf("%d , &testn");
+    for(int i=0; i < testn; i++)
+        solve();
+}
