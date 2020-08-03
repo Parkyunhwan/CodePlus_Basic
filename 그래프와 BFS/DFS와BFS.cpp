09a9d3490bb2189332ffc4b9c6c18746d242f3f0
YunHwan <pyh8618@gmail.com>
2020-08-03 23:53:22 +0900

    200803 DFS와BFS TimeOver

--- 그래프와 BFS/DFS와BFS.cpp
+++ 그래프와 BFS/DFS와BFS.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <memory.h>
 using namespace std;
-vector<int> v[1000];
-
+vector<int> v[1001];
+vector<int> result;
+bool Select[1001]={false,};
 int N,M,start;
-void DFS(int idx, int cnt)
+bool DFS(int idx, int cnt)
 {
     if(cnt==N){
-        return;
+        for(int i=0; i<result.size(); i++)
+            cout << result[i];
+        cout << "\n";
+        return true;
     }
-    for(int i=0; i < v[start].size(); i++){
-        
+    Select[idx] = true;
+    for(int i=0; i < v[idx].size(); i++){
+        int element = v[idx][i];
+        if(Select[element]==true) continue;
+            result.push_back(element);
+            if(DFS(element, cnt+1))
+                return true;
+            result.pop_back();
     }
-
+    Select[idx] = false;
+    return false;
+}
+bool BFS(int idx, int cnt)
+{
+    Select[idx]=true;
+    for(int i=0; i<v[idx].size(); i++){
+        int element = v[idx][i];
+        if(Select[element]==true) continue;
+        result.push_back(element);
+        if(result.size()==N){
+            for(int i=0; i< result.size(); i++)
+                cout << result[i] << " ";
+            cout << "\n";
+            return true;     
+        }  
+    }
+    BFS(result.back(), cnt+1);
 }
 int main()
 {
@@ -27,5 +55,10 @@ int main()
     }
     for(int i=0; i<N; i++)
         sort(v[i].begin(), v[i].end());
+    result.push_back(start);
     DFS(start,1);
+    memset(Select,false,sizeof(Select));
+    result.clear();
+    result.push_back(start);
+    BFS(start,1);
 }
